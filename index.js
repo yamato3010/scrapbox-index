fc17b734e0b726ad0c7e0ea921251af57d361ea5
DESKTOP-SD0H0H3\yama3 <yama3to10yama3@gmail.com>
2022-10-24 15:34:14 +0900

    google search console用

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get("/scrapbox/:title", (req, res) => {
     res.redirect(301, `https://scrapbox.io/yamato3010/${encodeURIComponent(title)}`);
 });
 
+app.use(express.static('public'));
+
 app.listen(PORT=process.env.PORT || 3000, function () {
     console.log("Node app is running at localhost:" + app.get('port'))
 });
