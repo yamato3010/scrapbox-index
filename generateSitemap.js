f3b865a3be7b0363dcd93ba12e9f5c4ec74698b0
DESKTOP-SD0H0H3\yama3 <yama3to10yama3@gmail.com>
2022-10-27 13:42:27 +0900

    generateSitemapを追加

---
+++ generateSitemap.js
@@ -0,0 +1,48 @@
+var client = require('cheerio-httpcli');
+var RSS = "https://scrapbox.io/api/feed/yamato3010";
+let fs = require('fs');
+
+function genSitemap() {
+    var RSSArr = [];
+    var map = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">"
+    client.fetch(RSS, {})
+        .then(function (result) {
+            if (result.error) { console.log("error"); return; }
+            result.$("item").each(function (idx) {
+                title = result.$(this).children("title").text();
+                address = result.$(this).children("link").text();
+                // console.log(title);
+                // console.log(address);
+                var key = result.$(this).children("link").text();
+                var keyArr = key.split("/");
+                var lastMod = result.$(this).children("pubDate").text();
+                var lastModDate = new Date(lastMod);
+                console.log(lastModDate);
+                var y = lastModDate.getFullYear();
+                var m = ('00' + (lastModDate.getMonth()+1)).slice(-2);
+                var d = ('00' + lastModDate.getDate()).slice(-2);
+                var url = "<sitemap><loc>https://yamato-scrapbox.herokuapp.com/scrapbox/" + keyArr[4]+"</loc><lastmod>" + y + '-' + m + '-' + d + "</lastmod></sitemap>";
+
+                // var url = "<url><loc>https://fast-castle-61290.herokuapp.com/scrapbox/" + keyArr[4]+"</loc><lastmod>" + lastMod + "</lastmod></url>";
+                var tmp = {
+                    [url]: {
+                        lastmod: '2018-04-01',
+                        changefreq: 'always',
+                        priority: 1.0,
+                    }
+                }
+                console.log(lastMod);
+                RSSArr.push(tmp);
+                map = map + url;
+            });
+            map = map + "<url><loc>https://fast-castle-61290.herokuapp.com/</loc></url><url><loc>https://fast-castle-61290.herokuapp.com/sitemap.xml</loc></url><url><loc>https://fast-castle-61290.herokuapp.com/scrapbox/:title</loc></url></urlset>"
+            // console.log(map);
+            fs.writeFile("sitemap.xml", map, (err) => {
+                if (err) throw err;
+                console.log('正常に書き込みが完了しました');
+            });
+        })
+        return(map);
+};
+
+genSitemap();
